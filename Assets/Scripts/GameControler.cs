d99207685fd2e8b8ba991776d7cf74b302abd18f
Starosadcha <s32262@pjwstk.edu.pl>
2025-05-05 13:51:09 +0200

    poxpg05.05(2)

--- Assets/Scripts/GameControler.cs
+++ Assets/Scripts/GameControler.cs
@@ -8,6 +8,7 @@ public class GameControler : MonoBehaviour
     public string Word ="after";
     public TextMeshProUGUI MainWord;
     public TextMeshProUGUI OurInput;
+    public TextMeshProUGUI SubmitedInput;
     public TMP_InputField InputField;
 
     // Start is called before the first frame update
@@ -19,6 +20,11 @@ public class GameControler : MonoBehaviour
     public void OnInputChanged(string input)
     {
         OurInput.text = input;
+    }
+    
+    public void OnClick ()
+    {
+
     }
         
 
