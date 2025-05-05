7d3a9a3ae8edf62407ca940fe3f3d5f567413c58
Starosadcha <s32262@pjwstk.edu.pl>
2025-05-05 13:50:11 +0200

    PoxpgCwicz05.05

---
+++ Assets/Scripts/GameControler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameControler : MonoBehaviour
+{
+    public string Word ="after";
+    public TextMeshProUGUI MainWord;
+    public TextMeshProUGUI OurInput;
+    public TMP_InputField InputField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainWord.text = Word;
+    }
+
+    public void OnInputChanged(string input)
+    {
+        OurInput.text = input;
+    }
+        
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
